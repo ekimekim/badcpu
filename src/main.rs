efeac8da3ef56bba56610026c1b6d4e259e01ed8
Mike Lang <mikelang3000@gmail.com>
2020-11-30 22:08:11 +1100

    wip:

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ mod cpu;
 mod memory;
 
 fn main() {
-
+	let mut memory = memory::NullMemory{};
+	let mut cpu = cpu::Cpu::new(&mut memory);
+	// null memory means these will always be nops (0x00 -> immd 0)
+	cpu.step();
 }
--- src/memory.rs
+++ src/memory.rs
@@ -3,3 +3,40 @@ pub trait Memory {
 	fn read(&self, bank: u8, addr: u8) -> u8;
 	fn write(&mut self, bank: u8, addr: u8, value: u8);
 }
+
+
+// A dummy memory target that ignores writes and always reads 0
+pub struct NullMemory {}
+
+impl Memory for NullMemory {
+	fn read(&self, _bank: u8, _addr: u8) -> u8 {
+		0
+	}
+	fn write(&mut self, _bank: u8, _addr: u8, _value: u8) {}
+}
+
+
+// A straightforward directly mapped memory, initialized to 0 if not otherwise given data
+pub struct SimpleMemory {
+	data: [u8; 65536],
+}
+
+impl Memory for SimpleMemory {
+	fn read(&self, bank: u8, addr: u8) -> u8 {
+		let index = ((bank as u16) << 8) | (addr as u16);
+		self.data[index]
+	}
+	fn write(&mut self, bank: u8, addr: u8, value: u8) {
+		let index = ((bank as u16) << 8) | (addr as u16);
+		self.data[index] = value;
+	}
+}
+
+impl SimpleMemory {
+	pub fn new(initial: &[u8]) -> Self {
+		let data = [0u8; 65536];
+		// This will panic if initial.len() > 65536
+		data[..initial.len()].copy_from_slice(initial);
+		SimpleMemory{data}
+	}
+}
