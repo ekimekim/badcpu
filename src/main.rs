0a2f8d19087a665e56aa7b63f642feefbb70f3fb
Mike Lang <mikelang3000@gmail.com>
2020-12-03 03:54:06 +1100

    wip:

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,17 @@ mod memory;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	if args.len() < 2 {
-		eprintln!("Usage: {} ROM", &args[0]);
+	if args.len() < 3 {
+		eprintln!("Usage: {} ROM STEPS", &args[0]);
 		return;
 	}
 	let rom = fs::read(&args[1]).unwrap();
+	let steps: u32 = args[2].parse().unwrap();
 	let mut memory = memory::SimpleMemory::new(&rom);
 	let mut cpu = cpu::Cpu::new(&mut memory);
-	println!("Before: {:?}", &cpu);
-	cpu.step();
-	println!("After: {:?}", &cpu);
+	println!("Initial state: {:?}", &cpu);
+	for step in 0..steps {
+		cpu.step();
+		println!("After step {}: {:?}", step, &cpu);
+	}
 }
