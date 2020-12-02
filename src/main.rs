26fc71e5a68b15f84de51348c23f990f82199303
Mike Lang <mikelang3000@gmail.com>
2020-12-03 03:37:31 +1100

    wip: read rom from file on arg1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use std::env;
+use std::fs;
+
 mod cpu;
 mod memory;
 
 fn main() {
-	let mut memory = memory::NullMemory{};
+	let args: Vec<String> = env::args().collect();
+	if args.len() < 2 {
+		println!("Usage: {} ROM", &args[0]);
+		return;
+	}
+	let rom = fs::read(args[1]).unwrap();
+	let mut memory = memory::SimpleMemory::new(rom);
 	let mut cpu = cpu::Cpu::new(&mut memory);
 	// null memory means these will always be nops (0x00 -> immd 0)
 	cpu.step();
